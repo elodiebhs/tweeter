025701188e287bb55bdd94298b73d1e63f86cafd
elodiebhs <elodiebouthors@hotmail.com>
2021-02-24 03:47:18 +0000

    updates

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,13 @@
 
 //test that it is being loaded into the index.html file correctly.
 $(document).ready(function() {
-  console.log ("this is working correctly")
-//exemple with button
-  $("#btn").on('click', function() {
-    console.log(this); //The this keyword is a reference to the button
-  });
   
   $("#tweet-text").on('keypress', function() {
-   //console.log($(this)); 
-   //Calculating the num of Character length minus the max allowed
-    let numOfCharacters=$(this).val().length;
-    let remainCharacters = 140 - numOfCharacters;
-    //console.log(remainCharacters);
+    //Calculating the num of Character length minus the max allowed
+    const numOfCharacters=$(this).val().length;
+    const remainCharacters = 140 - numOfCharacters;
     //Traverse up the DOM tree until counter
-    let characterOfCounter = $(this).parent().next().find(".counter");
-    console.log(characterOfCounter);
+    const characterOfCounter = $(this).parent().next().find(".counter");
     characterOfCounter.html(remainCharacters);
     //Counter turning red if max characters 
     if(remainCharacters < 0){
@@ -23,8 +15,6 @@ $(document).ready(function() {
     } else {
       characterOfCounter.removeClass("counterRed")
     }
-
   })
-
 });
 
