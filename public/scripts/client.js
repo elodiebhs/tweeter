41eeddd10ebf51c2fc50e10b88b1987380a254fa
elodiebhs <elodiebouthors@hotmail.com>
2021-02-24 23:20:52 +0000

    Update Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,31 +10,6 @@
 $( document ).ready(function() {
   
 
-// Fake data taken from initial-tweets.json
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 //Create the HTML for a tweet
 const createTweetElement = (tweet) => {
@@ -66,14 +41,25 @@ const createTweetElement = (tweet) => {
 
 //Lopp through the tweets ans appends the container with tweets
 const renderTweets = function(tweets) {
+  $('#tweet-container').empty()
   // loops through tweets
-  for (let element of tweets){
+  for (let element of tweets) {
     // calls createTweetElement for each tweet
     const result = createTweetElement(element);
-    $('#tweet-container').append(result)
+    $('#tweet-container').prepend(result)
   }
   
 }
+
+//loads tweets from the server using AJAX - use AJAX to fetch (GET) data from the server.
+const loadtweets = function() {
+  //$.get(URL,callback);
+ $.get('/tweets', function(tweets) {
+   renderTweets(tweets);
+ })
+}
+loadtweets()
+
   // takes return value and appends it to the tweets container
   //renderTweets()
 
@@ -86,7 +72,7 @@ console.log($tweet); // to see what it looks like
 //$('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 */
   //Write a function actually submit the data.
-  $('#submittweet').on('click',function(event){
+  $('#submittweet').on('click',function(event) {
     event.preventDefault();
     let tweetText = $("#tweet-text").val();
     let tweetData = {
@@ -96,31 +82,16 @@ console.log($tweet); // to see what it looks like
       method: 'POST',
       url: '/tweets',
       data: tweetData,
-      success:function(result){
+      success:function(result) {
         //console.log("The post was successful");
-        getAllTweets();
+        loadtweets();
       },
-      error: function(err){
+      error: function(err) {
         console.log("There was an error in the ajax call",err);
       }
     });
   });
 
-  function getAllTweets(){
-    $.ajax({
-      method: 'GET',
-      url: '/tweets',
-      success:function(result){
-        //console.log("The tweets fetched ",result);
-        renderTweets(result);
-      },
-      error: function(err){
-        console.log("There was an error in the ajax call",err);
-      }
-    });
-  }
-
-  //We are going to call this function
-  getAllTweets();
-
 });
+
+
